ffea5612bc57f77b7f3d0eaa0ebec5e67c768c79
mcalidguid <marifelcalidguid29@gmail.com>
2020-08-27 02:11:50 +0800

    modified print messages

--- birthday_details.py
+++ birthday_details.py
@@ -17,18 +17,18 @@ class BirthdayDetail:
     def find_age(self, birth_date):
         try:
             # The number of days in a year
-            days_in_year = 365.2425
+            total_days_in_year = 365.2425
             # Convert the user input to date() format
             born = datetime.datetime.strptime(birth_date, "%d%m%Y").date()
             # Subtract the date today to the date of birth then divide it to the exact number of days in a year
-            age = int((date.today() - born).days / days_in_year)
-            print("As of today, you are now", age, "years old.")
+            age = int((date.today() - born).days / total_days_in_year)
+            print("As of today, you are now", age, "year/s old.")
         except ValueError:
-            print("Please try Again.")
+            print("Please try again.")
 
 
 day_dob = BirthdayDetail()
-print("Enter your Birthday [DDMMYYY]: ")
+print("Enter your Birthday [DDMMYYYY]: ")
 user_input = input(">>>: ")
 day_dob.find_day(user_input)
 day_dob.find_age(user_input)
